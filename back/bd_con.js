2b16be4772ae2f60728153be20880b5bfdf925d7
Bemmo <bemmoFulbert@gmail.com>
2023-07-13 18:15:20 +0100

    formulaire client-artisan

--- back/bd_con.js
+++ back/bd_con.js
@@ -8,8 +8,8 @@ const info = {
 }
 const pg_promise = require('pg-promise')
 const pgp = pg_promise()
-const dbPhrase =  'postgres://'+info.username+":"+info.password+"@"+info.host+":"+info.port+"/"+info.database
-//'postgresql://oneclickwork4300:YyOM3xILEn4u@ep-lucky-wave-650666.us-west-2.aws.neon.tech/ocw?ssl='+info.ssl
+const dbPhrase =  //'postgres://'+info.username+":"+info.password+"@"+info.host+":"+info.port+"/"+info.database
+'postgresql://oneclickwork4300:YyOM3xILEn4u@ep-lucky-wave-650666.us-west-2.aws.neon.tech/ocw?ssl='+info.ssl
 const db = pgp(dbPhrase)
 console.log ("connexion a : "+ dbPhrase)
 
